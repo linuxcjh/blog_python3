db9212c6ccb223219352d0ad31b4a5e322f1480d
chenjianhui <e.cjh.cn@gmail.com>
2017-06-25 16:38:35 +0800

    modify settins

--- blogporject/settings.py
+++ blogporject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1@@2ye*-w^5qiemn8abmq%h6&zj=9()baazo1(e*-tc1&1_biv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.ialps.org']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
